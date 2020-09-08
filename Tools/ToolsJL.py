7dc64ff9b4dac440cb513a93c1b844b08765fb1c
WolfyWolf33 <wolfywolfwolf33@gmail.com>
2020-09-08 14:07:29 -0400

    first push

--- Tools/ToolsJL.py
+++ Tools/ToolsJL.py
@@ -15,3 +15,23 @@ print(isEven(9))
 
 for i in range (0, 100, 1):
     print(str(i)+" is "+ str(isEven(i)))
+
+
+def missing_char(str, n):
+
+    #solution 1
+    '''
+    newStr = ""
+    newStr = str[0:n] + str[n + 1: len(str)]
+    return newStr
+    '''
+
+
+    front = str[:n]   # up to but not including n
+    back = str[n+1:]  # n+1 through end of string
+    return front + back
+
+    #missing_char('kitten', 1) → 'ktten'
+    #missing_char('kitten', 0) → 'itten'
+    #missing_char('kitten', 4) → 'kittn'
+
