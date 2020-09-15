e077d66438dc4d20e27d41a3bec4df1659819946
WolfyWolf33 <wolfywolfwolf33@gmail.com>
2020-09-14 23:19:53 -0400

    Conversion and piano projects finished

--- binaryconversion/binary.py
+++ binaryconversion/binary.py
@@ -4,26 +4,55 @@ print("Stage 1")
 
 
 def process(*args):
-	print("process")
+	
 
 	val = ent_value.get()
-	print(val)
+	
 
 
 
 #check to make sure it is string of 1's and 0's
+	check = check01(val)
 
 
+	if (check == True):
+		val = remove0(val)
+		result = base2to10(val)
+		lab_results.config(text = str(val) + " --->" + str(result))
+	else:
+		lab_results.config(text = "Invalid")
 
-#if val is valid
-	# remove left most -
-	#conversions
-	#update display with conversion
-#else:
-	#update dispaly wiht error message.
+
+	ent_value.delete(0,tk.END)
+
+def base2to10(str):
+
+	n = 0
+	total = 0
+
+	for i in range(len(str) - 1, -1, -1):
+		total = total + int(str[i]) * 2**n
+		n = n + 1
+
+	return total
+
+def remove0(str):
+
+	for i in range(0, len(str), 1):
+		if (str[i] == "1"):
+			return str[i:]
 
 
-	ent_value.delete(0,tk.END)	
+	return str
+
+def check01(str):
+	num_0 = str.count("0")
+	num_1 = str.count("1")
+
+	if num_0 + num_1 == len(str):
+		return True
+	return False
+
 
 
 root = tk.Tk()
